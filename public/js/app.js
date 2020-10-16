0cb1cc935fe962b59f69d12b729a43183622afc4
Diego Pena <diego@Diegos-MacBook-Air.local>
2020-10-16 18:06:17 -0500

    please fucking work

--- public/js/app.js
+++ public/js/app.js
@@ -17,7 +17,7 @@ weatherForm.addEventListener('submit', (e) => {
     response.json().then((data) => {
         if(data.error){
             messageOne.textContent = data.error
-            messageTwo.textContent = ""
+            messageTwo.textContent = " "
         }
         else {
             messageOne.textContent = data.location
